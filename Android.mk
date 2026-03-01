4cafce9ded3296aa623c74272ce427d96d70ce77
whzzxc <jamakasi.koor@gmail.com>
2026-03-01 08:38:09 +0500

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -53,6 +53,6 @@ FILE_LIST += $(wildcard $(LOCAL_PATH)/vendor/imgui/*.cpp)
 FILE_LIST += $(wildcard $(LOCAL_PATH)/vendor/hash/md5.cpp)
 
 LOCAL_SRC_FILES := $(FILE_LIST:$(LOCAL_PATH)/%=%)
-LOCAL_STATIC_LIBRARIES := libopenal libopus libenet
+LOCAL_STATIC_LIBRARIES := libopenal libopus libenet raknet
 LOCAL_CPPFLAGS := -w -s -fvisibility=hidden -pthread -Wall -fpack-struct=1 -O2 -std=c++14 -fexceptions
 include $(BUILD_SHARED_LIBRARY)
